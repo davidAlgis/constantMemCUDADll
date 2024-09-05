cd5f2a0fbad738c4c07ef043fe681cb806467dee
davidAlgis <david.algis@tutamail.com>
2024-09-05 13:04:32 +0200

    Reformat files

--- Utilities/cuda_include.h
+++ Utilities/cuda_include.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "cuda_runtime.h"
-#include <stdexcept>
 #include <iostream>
+#include <stdexcept>
 /**
  * @brief      Check if a cuda function has succeed and log the cuda error if
  * it's not the case
@@ -21,11 +21,11 @@
  *
  */
 #define CUDA_CHECK_RETURN(ans)                                                 \
-  {                                                                            \
-    int ret = cudaAssert((ans), __FILE__, __LINE__);                           \
-    if (ret != 0)                                                              \
-      return ret;                                                              \
-  }
+    {                                                                          \
+        int ret = cudaAssert((ans), __FILE__, __LINE__);                       \
+        if (ret != 0)                                                          \
+            return ret;                                                        \
+    }
 
 /**
  * @brief      Check if a cuda function has succeed. If it doesn't log the cuda
@@ -36,13 +36,14 @@
  *
  */
 #define CUDA_CHECK_RETURN_VOID(ans, msg)                                       \
-  {                                                                            \
-    int ret = cudaAssert((ans), __FILE__, __LINE__);                           \
-    if (ret != 0) {                                                            \
-      Log::log().debugLogError(msg);                                           \
-      return;                                                                  \
-    }                                                                          \
-  }
+    {                                                                          \
+        int ret = cudaAssert((ans), __FILE__, __LINE__);                       \
+        if (ret != 0)                                                          \
+        {                                                                      \
+            Log::log().debugLogError(msg);                                     \
+            return;                                                            \
+        }                                                                      \
+    }
 
 /**
  * @brief      Check if a cuda function has succeed. If it doesn't log the cuda
@@ -52,12 +53,13 @@
  *
  */
 #define CUDA_CHECK_RETURN_BOOL(ans)                                            \
-  {                                                                            \
-    int ret = cudaAssert((ans), __FILE__, __LINE__);                           \
-    if (ret != 0) {                                                            \
-      return false;                                                            \
-    }                                                                          \
-  }
+    {                                                                          \
+        int ret = cudaAssert((ans), __FILE__, __LINE__);                       \
+        if (ret != 0)                                                          \
+        {                                                                      \
+            return false;                                                      \
+        }                                                                      \
+    }
 
 /**
  * @brief      Check if a cuda function has succeed. If it doesn't log the
@@ -66,11 +68,11 @@
  * @param      ans   A function that return a cudaError_t
  */
 #define CUDA_CHECK_THROW(ans)                                                  \
-  {                                                                            \
-    int ret = cudaAssert((ans), __FILE__, __LINE__);                           \
-    if (ret != 0)                                                              \
-      throw std::runtime_error(cudaGetErrorString(ans));                       \
-  }
+    {                                                                          \
+        int ret = cudaAssert((ans), __FILE__, __LINE__);                       \
+        if (ret != 0)                                                          \
+            throw std::runtime_error(cudaGetErrorString(ans));                 \
+    }
 
 /**
  * @brief      Log an error associated to cuda library if there has been an
@@ -82,13 +84,15 @@
  *
  * @return     the return code
  */
-inline int cudaAssert(cudaError_t code, const char *file, int line) {
-  if (code != cudaSuccess) {
-    char buffer[2048];
-    snprintf(buffer, sizeof(buffer), "Cuda error: %i %s %s %d\n", code,
-             cudaGetErrorString(code), file, line);
-    std::string strError(buffer);
-    std::cout << buffer << '\n';
-  }
-  return (int)code;
+inline int cudaAssert(cudaError_t code, const char *file, int line)
+{
+    if (code != cudaSuccess)
+    {
+        char buffer[2048];
+        snprintf(buffer, sizeof(buffer), "Cuda error: %i %s %s %d\n", code,
+                 cudaGetErrorString(code), file, line);
+        std::string strError(buffer);
+        std::cout << buffer << '\n';
+    }
+    return (int)code;
 }
